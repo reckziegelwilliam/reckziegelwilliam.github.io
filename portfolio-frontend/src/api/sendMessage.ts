8b38e5328edcbfec6382c6b1bea51ca5f402954f
Liam <reckziegelwilliam96@gmail.com>
2023-09-04 13:36:34 -0700

    Updates

--- portfolio-frontend/src/api/sendMessage.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const sendMessage = async (data: any) => {
-  const response = await axios.post(process.env.BACKEND_URL + '/contact', data);
-  return response.data;
-};
